1086d26630b8860026922d731badb94535633ebd
Stevenson  Lennox <steve148@ug151.eecg>
2016-11-19 16:28:11 -0500

    Use original randtrack file for tm case

--- hw4/randtrack_tm.cc
+++ hw4/randtrack_tm.cc
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+
 #include "defs.h"
 #include "hash.h"
 
@@ -41,63 +41,18 @@ class sample {
   void print(FILE *f){printf("%d %d\n",my_key,count);}
 };
 
-typedef struct {
-        int start;
-        int end;
-} thread_args;
-
 // This instantiates an empty hash table
 // it is a C++ template, which means we define the types for
 // the element and key value here: element is "class sample" and
 // key value is "unsigned".  
 hash<sample,unsigned> h;
 
-pthread_mutex_t global_lock;
-pthread_t tid[4];
-
-void* sample_shit(void *args) {
+int  
+main (int argc, char* argv[]){
   int i,j,k;
   int rnum;
   unsigned key;
   sample *s;
-
-  // process streams starting with different initial numbers
-  thread_args bounds = *((thread_args *) args);
-  free(args);
-  for (i = bounds.start; i <= bounds.end; i++){
-    rnum = i;
-
-    // collect a number of samples
-    for (j=0; j<SAMPLES_TO_COLLECT; j++){
-
-      // skip a number of samples
-      for (k=0; k<samples_to_skip; k++){
-	rnum = rand_r((unsigned int*)&rnum);
-      }
-
-      // force the sample to be within the range of 0..RAND_NUM_UPPER_BOUND-1
-      key = rnum % RAND_NUM_UPPER_BOUND;
-      
-      pthread_mutex_lock(&global_lock);
-      // if this sample has not been counted before
-      if (!(s = h.lookup(key))){
-	
-	// insert a new element for it into the hash table
-	s = new sample(key);
-	h.insert(s);
-      }
-
-      // increment the count for the sample
-      s->count++;
-      pthread_mutex_unlock(&global_lock);
-    }
-  }
-  return NULL;
-}
-
-int main (int argc, char* argv[]){
-  int i;
-  int err;
 
   // Print out team information
   printf( "Team Name: %s\n", team.team );
@@ -121,54 +76,34 @@ int main (int argc, char* argv[]){
 
   // initialize a 16K-entry (2**14) hash of empty lists
   h.setup(14);
-  
-  pthread_mutex_init(&global_lock,NULL);
-  
-  if (num_threads == 1) {
-        thread_args* bounds = (thread_args *) malloc(sizeof(thread_args));
-        bounds->start = 0 ;
-        bounds->end = 3;
-        err = pthread_create(&tid[0], NULL, &sample_shit, bounds);
+
+  // process streams starting with different initial numbers
+  for (i=0; i<NUM_SEED_STREAMS; i++){
+    rnum = i;
+
+    // collect a number of samples
+    for (j=0; j<SAMPLES_TO_COLLECT; j++){
+
+      // skip a number of samples
+      for (k=0; k<samples_to_skip; k++){
+	rnum = rand_r((unsigned int*)&rnum);
+      }
+
+      // force the sample to be within the range of 0..RAND_NUM_UPPER_BOUND-1
+      key = rnum % RAND_NUM_UPPER_BOUND;
+
+      // if this sample has not been counted before
+      if (!(s = h.lookup(key))){
+	
+	// insert a new element for it into the hash table
+	s = new sample(key);
+	h.insert(s);
+      }
+
+      // increment the count for the sample
+      s->count++;
+    }
   }
-  else if (num_threads == 2) {
-        thread_args *bounds1 = (thread_args *) malloc(sizeof(thread_args));
-        bounds1->start = 0 ;
-        bounds1->end = 1;
-        
-        thread_args *bounds2 = (thread_args *) malloc(sizeof(thread_args));
-        bounds2->start = 2 ;
-        bounds2->end = 3;
-        err = pthread_create(&tid[0], NULL, &sample_shit, (void *) bounds1);
-        err = pthread_create(&tid[1], NULL, &sample_shit, (void *) bounds2);
-        
-  }
-  else if (num_threads == 4) {
-        thread_args *bounds1 = (thread_args *) malloc(sizeof(thread_args));
-        bounds1->start = 0 ;
-        bounds1->end = 0;
-        
-        thread_args *bounds2 = (thread_args *) malloc(sizeof(thread_args));
-        bounds2->start = 1 ;
-        bounds2->end = 1;
-        
-        thread_args *bounds3 = (thread_args *) malloc(sizeof(thread_args));
-        bounds3->start = 2 ;
-        bounds3->end = 2;
-        
-        thread_args *bounds4 = (thread_args *) malloc(sizeof(thread_args));
-        bounds4->start = 3 ;
-        bounds4->end = 3;
-        
-        err = pthread_create(&tid[0], NULL, &sample_shit, (void *) bounds1);
-        err = pthread_create(&tid[1], NULL, &sample_shit, (void *) bounds2);
-        err = pthread_create(&tid[2], NULL, &sample_shit, (void *) bounds3);
-        err = pthread_create(&tid[3], NULL, &sample_shit, (void *) bounds4);
-  }
-  
-        // wait for all threads to be done
-        for(i = 0; i < num_threads; i++) {
-                pthread_join(tid[i], NULL);
-        }
 
   // print a list of the frequency of all samples
   h.print();
